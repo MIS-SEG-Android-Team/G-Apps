6747aefdedd800da5ccbc4a3f9d754d1c8b68ed6
ggutoman <ggutoman@gmail.com>
2024-10-07 17:25:56 +0800

    KITA MOTO - Referral Program #4
    added label identification for required and optional fields

--- g3appdriver/src/main/java/org/rmj/g3appdriver/lib/Ganado/pojo/ClientInfo.java
+++ g3appdriver/src/main/java/org/rmj/g3appdriver/lib/Ganado/pojo/ClientInfo.java
@@ -189,16 +189,6 @@ public class ClientInfo {
             return false;
         }
 
-        /*if(dBirthDte.isEmpty()){
-            message = "Please enter birth date.";
-            return false;
-        }
-
-        if(sBirthPlc.isEmpty()){
-            message = "Please enter birth place.";
-            return false;
-        }*/
-
         if(sTownIDxx.isEmpty()){
             message = "Please select town or municipality.";
             return false;
@@ -215,10 +205,6 @@ public class ClientInfo {
             return false;
         }
 
-        /*if(sEmailAdd.isEmpty()){
-            message = "Please enter email address.";
-            return false;
-        }*/
         if(sReltionx.isEmpty()){
             message = "Please select relationship.";
             return false;
