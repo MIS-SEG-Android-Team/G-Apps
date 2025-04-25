767c02014c20fa1cfadae30656b0a034d90b9254
ggutoman <ggutoman@gmail.com>
2025-04-25 10:40:49 +0800

    Dashboard Event Fiesta #15
    added app introduction on dashboard before login

--- app/src/main/java/org/rmj/guanzongroup/gconnect/Fragment/Fragment_Dashboard.java
+++ app/src/main/java/org/rmj/guanzongroup/gconnect/Fragment/Fragment_Dashboard.java
@@ -9,12 +9,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.Observer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.imageview.ShapeableImageView;
+import com.google.android.material.textview.MaterialTextView;
 
 import org.rmj.g3appdriver.dev.Database.Entities.EClientInfo;
 import org.rmj.g3appdriver.dev.Repositories.RClientInfo;
@@ -28,9 +32,24 @@ public class Fragment_Dashboard extends Fragment {
 
     private View view;
 
+    //todo objects displayed after login
+    public MaterialCardView mcv_BotNav;
     public BottomNavigationView botNav;
     public ViewPager2 viewPager;
     public Fragment_Poll loPoll = new Fragment_Poll();
+
+    //todo objects displayed before login
+    private ConstraintLayout layout_intro;
+    private ShapeableImageView logo;
+    private ShapeableImageView btnNext;
+    private MaterialCardView mcv_intro;
+    private MaterialTextView mtv_text;
+    private ShapeableImageView siv_icon;
+
+    private ShapeableImageView end_logo;
+    private MaterialTextView mtv_instruct;
+
+    private MutableLiveData<Integer> pageCount = new MutableLiveData<>();
 
     private RClientInfo loClient;
 
@@ -49,14 +68,26 @@ public class Fragment_Dashboard extends Fragment {
         initViews(); //todo init views
         initListener(); //todo init listener
         initObservables(); //todo init observables
-        setupPages(); // todo init pages
 
         return view;
     }
     private void initViews() {
 
+        //todo objects displayed before login
         viewPager = view.findViewById(R.id.viewpager);
+        mcv_BotNav = view.findViewById(R.id.coordinatorLayout);
         botNav = view.findViewById(R.id.bottom_navigation);
+
+        //todo objects displayed after login
+        layout_intro = view.findViewById(R.id.layout_intro);
+        logo = view.findViewById(R.id.logo);
+        btnNext = view.findViewById(R.id.btnNext);
+        mcv_intro = view.findViewById(R.id.mcv_intro);
+        mtv_text = view.findViewById(R.id.mtv_text);
+        siv_icon = view.findViewById(R.id.siv_icon);
+
+        end_logo = view.findViewById(R.id.end_logo);
+        mtv_instruct = view.findViewById(R.id.mtv_instruct);
 
         botNav.setBackground(null);
     }
@@ -74,6 +105,18 @@ public class Fragment_Dashboard extends Fragment {
             }
             return true;
         });
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!pageCount.isInitialized()){
+                    pageCount.setValue(1);
+                }else {
+                    pageCount.setValue(pageCount.getValue() + 1);
+                }
+            }
+        });
     }
     private void initObservables() {
 
@@ -83,17 +126,82 @@ public class Fragment_Dashboard extends Fragment {
 
                 if (eClientInfo != null){
                     //botNav.getMenu().findItem(R.id.nav_Bingo).setVisible(true); todo: for future use, postponed
+                    mcv_BotNav.setVisibility(View.VISIBLE);
                     botNav.getMenu().findItem(R.id.nav_Barcode).setVisible(true);
                     botNav.getMenu().findItem(R.id.nav_Poll).setVisible(true);
+
+                    layout_intro.setVisibility(View.GONE);
+
+                    setupPages(); // todo init pages
+
                 }else {
                     //botNav.getMenu().findItem(R.id.nav_Bingo).setVisible(false); todo: for future use, postponed
+                    mcv_BotNav.setVisibility(View.GONE);
                     botNav.getMenu().findItem(R.id.nav_Barcode).setVisible(false);
                     botNav.getMenu().findItem(R.id.nav_Poll).setVisible(false);
 
                     viewPager.setCurrentItem(0);
+
+                    layout_intro.setVisibility(View.VISIBLE);
+                    btnNext.setOutlineProvider(null);
+
+                    pageCount.observe(getViewLifecycleOwner(), new Observer<Integer>() {
+                        @Override
+                        public void onChanged(Integer integer) {
+                            initIntroPages(integer);
+                        }
+                    });
+
                 }
             }
         });
+    }
+    private void initIntroPages(Integer page){
+
+        Log.d("Count", String.valueOf(page));
+
+        switch (page){
+
+            case 1:
+                logo.setVisibility(View.VISIBLE);
+                mcv_intro.setVisibility(View.VISIBLE);
+                btnNext.setImageResource(R.drawable.arrow2);
+
+                mtv_text.setText("Browse your future RIDE");
+                siv_icon.setImageResource(R.drawable.kay1);
+                break;
+
+            case 2:
+                mtv_text.setText("Selfies with your new PHONE");
+                siv_icon.setImageResource(R.drawable.kay2);
+                break;
+
+            case 3:
+                mtv_text.setText("Be updated to our Promos and Events");
+                siv_icon.setImageResource(R.drawable.kay3);
+                break;
+
+            case 4:
+                mtv_text.setText("Start\nGuanzon Connect");
+                siv_icon.setImageResource(R.drawable.kay4);
+                break;
+
+            case 5:
+                logo.setVisibility(View.GONE);
+                btnNext.setVisibility(View.GONE);
+                mcv_intro.setVisibility(View.GONE);
+
+                end_logo.setVisibility(View.VISIBLE);
+                mtv_instruct.setVisibility(View.VISIBLE);
+
+                break;
+
+            default:
+                mcv_intro.setVisibility(View.VISIBLE);
+                mtv_text.setText("Browse your future RIDE");
+                siv_icon.setImageResource(R.drawable.kay1);
+                break;
+        }
     }
     @SuppressLint("NotifyDataSetChanged")
     private void setupPages(){
